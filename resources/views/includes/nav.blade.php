f06d602de666135b1f3fcf0921b2697d29963570
abdulbasit-dev <abdullbasit99salah@gmail.com>
2022-01-05 12:07:35 +0300

    home page design

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,6 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container">
-        <a class="navbar-brand" href="{{ route('home') }}">{{ __('lang.nav.brand') }}</a>
+        <a class="navbar-brand" href="{{ route('home') }}"><img src="{{ asset('images/brand.png') }}"
+                width="30" height="auto"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,34 +9,44 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link fw-medium" aria-current="page" href="{{ route('home') }}">Home</a>
+                    <a class="nav-link fw-medium {{ Route::currentRouteName() == 'home' ? 'active' : '' }}"
+                        aria-current="page" href="{{ route('home') }}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('conference') }}">Conference and Grant</a>
+                    <a class="nav-link fw-medium {{ Route::currentRouteName() == 'conference' ? 'active' : '' }}"
+                        href="{{ route('conference') }}">Conference and Grant</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('graduateProject') }}">Graduate Project</a>
+                    <a class="nav-link fw-medium {{ Route::currentRouteName() == 'graduateProject' ? 'active' : '' }}"
+                        href="{{ route('graduateProject') }}">Graduate Project</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('evaluation') }}">Evaluation</a>
+                    <a class="nav-link fw-medium {{ Route::currentRouteName() == 'evaluation' ? 'active' : '' }}"
+                        href="{{ route('evaluation') }}">Evaluation</a>
                 </li>
             </ul>
-            <ul>
 
-                @if (Route::has('login'))
-                    <div class="top-right links">
-                        @auth
-                            <a href="{{ url('/home') }}">Home</a>
-                        @else
-                            <a href="{{ route('login') }}">Login</a>
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    <ul class="navbar-nav">
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">Login <i
+                                        class="fas fa-sign-in-alt"></i></a>
+                            </li>
 
                             @if (Route::has('register'))
-                                <a href="{{ route('register') }}">Register</a>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">Register <i
+                                            class="fas fa-user"></i></a>
+
+                                </li>
                             @endif
-                        @endauth
-                    </div>
-                @endif
-            </ul>
+                        @endguest
+
+                    </ul>
+                </div>
+            @endif
         </div>
     </div>
 </nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,23 +5,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <!-- BootStrap -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
 
-        {{-- custome style  --}}
-        <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
 
-        @if (config('app.locale') == 'ar' || config('app.locale') == 'kr')
-        <link href="{{ asset('/css/bootstrap-rtl.css') }}"
-          rel="stylesheet">
-      @endif
-    <title>{{ _('No Name') }}</title>
+    {{-- custome style --}}
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
+    @if (config('app.locale') == 'ar' || config('app.locale') == 'kr')
+        <link href="{{ asset('/css/bootstrap-rtl.css') }}" rel="stylesheet">
+    @endif
+    <title>{{ _('Graduta Projects') }}</title>
 </head>
 
 <body>
     {{-- Navbar --}}
     @include('includes.nav')
     @yield('content')
+
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    @stack('scripts')
+
 </body>
 
 </html>
