78f9c992de883b2e809c7a432ae7954a8c350c91
abdulbasit-dev <abdullbasit99salah@gmail.com>
2022-01-06 06:07:35 +0300

    add database file

---
+++ laravel_graduate_project.sql
@@ -0,0 +1,506 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 06, 2022 at 04:07 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.4.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laravel_graduate_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class_types`
+--
+
+CREATE TABLE `class_types` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `dept_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `class_types`
+--
+
+INSERT INTO `class_types` (`id`, `dept_id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 6, 'CS', '2022-01-05 15:54:29', '2022-01-05 16:27:15'),
+(2, 6, 'IT', '2022-01-05 15:54:29', '2022-01-05 15:54:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `colleges`
+--
+
+CREATE TABLE `colleges` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `colleges`
+--
+
+INSERT INTO `colleges` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Science', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(2, 'Engineering', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(3, 'Agriculture Engineering Sciences', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(4, 'Education', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(5, 'Arts', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(6, 'Languages', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(7, 'Administration and Economic', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(8, 'Law', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(9, 'Political Science', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(10, 'Basic Education', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(11, 'Physical Education & Sport Sciences', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(12, 'Islamic Sciences', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(13, 'Fine Arts', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(14, 'Education of Shaqlawa', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(15, 'Education of Makhmoor', '2022-01-05 15:54:29', '2022-01-05 15:54:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+
+CREATE TABLE `departments` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `college_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `departments`
+--
+
+INSERT INTO `departments` (`id`, `college_id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Biology Department', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(2, 1, 'Chemistry Department', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(3, 1, 'Physics Department', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(4, 1, 'Mathematics Department', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(5, 1, 'Geology Department', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(6, 1, 'Computer Science and IT Department', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(7, 1, 'Environmental Health and Science Department', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(8, 2, 'Civil Engineering Department', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(9, 2, 'Electrical Engineering Department', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(10, 2, 'Mechanical and Mechatronics Engineering', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(11, 2, 'Architectural Engineering Department', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(12, 2, 'Software and Informatics Engineering', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(13, 2, 'Water Resources Engineering', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(14, 2, 'Geomatics (Surveying) Engineering', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(15, 2, 'Chemical and Petrochemical Engineering', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(16, 2, 'Aviation Engineering Department', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(17, 4, 'Chemistry', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(18, 4, 'Mathematics', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(19, 4, 'Physics', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(20, 4, 'Biology', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(21, 4, 'Kurdish Language', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(22, 4, 'English Language', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(23, 4, 'Syriac Language', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(24, 4, 'Arabic Language', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(25, 4, 'Special Education', '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(26, 4, 'Educational and Psychological Counselings', '2022-01-05 15:54:29', '2022-01-05 15:54:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2022_01_04_122545_create_permission_tables', 1),
+(6, '2022_01_05_134244_create_colleges_table', 1),
+(7, '2022_01_05_134309_create_departments_table', 1),
+(8, '2022_01_05_134325_create_class_types_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `model_has_permissions`
+--
+
+CREATE TABLE `model_has_permissions` (
+  `permission_id` bigint(20) UNSIGNED NOT NULL,
+  `model_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `model_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `model_has_roles`
+--
+
+CREATE TABLE `model_has_roles` (
+  `role_id` bigint(20) UNSIGNED NOT NULL,
+  `model_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `model_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `model_has_roles`
+--
+
+INSERT INTO `model_has_roles` (`role_id`, `model_type`, `model_id`) VALUES
+(1, 'App\\Models\\User', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+
+CREATE TABLE `permissions` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `guard_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
+(1, 'users_manage', 'web', '2022-01-05 15:54:28', '2022-01-05 15:54:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `guard_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
+(1, 'administrator', 'web', '2022-01-05 15:54:28', '2022-01-05 15:54:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_has_permissions`
+--
+
+CREATE TABLE `role_has_permissions` (
+  `permission_id` bigint(20) UNSIGNED NOT NULL,
+  `role_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `role_has_permissions`
+--
+
+INSERT INTO `role_has_permissions` (`permission_id`, `role_id`) VALUES
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `user_type` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `user_type`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'admin@admin.com', NULL, NULL, '$2y$10$VBVkAxy4PJ1dxzgwfVsFsOm4D5xxj1fGl.SYIDcmCPvp5Xqz3eMr6', NULL, '2022-01-05 15:54:29', '2022-01-05 15:54:29'),
+(2, 'Kevyn Griffith', 'zasaqoqu@mailinator.com', NULL, 'Student', '$2y$10$2JXOCBoFcnxf4yjMBPfDiOq1iCwJWhNGMnzw0jB0tDUiavrlmoHjq', NULL, '2022-01-05 15:54:38', '2022-01-05 15:54:38'),
+(3, 'Thane Pearson', 'fiwe@mailinator.com', NULL, 'Head Of Department', '$2y$10$nn0UEHEmPUnTJmIBRb5LdeSsZrJ4Q3k28myMVNnD90jppNKWSEYY2', NULL, '2022-01-05 16:25:18', '2022-01-05 16:25:18'),
+(4, 'Sawyer Tyson', 'pixugite@mailinator.com', NULL, 'Department Rapporteur', '$2y$10$sLijxeffcu1s0cYbExiX4.XCrMBUFmmaQKzfDZl9ud5uzBSX.xI.S', NULL, '2022-01-05 16:33:46', '2022-01-05 16:33:46');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `class_types`
+--
+ALTER TABLE `class_types`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `class_types_dept_id_foreign` (`dept_id`);
+
+--
+-- Indexes for table `colleges`
+--
+ALTER TABLE `colleges`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `departments_college_id_foreign` (`college_id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `model_has_permissions`
+--
+ALTER TABLE `model_has_permissions`
+  ADD PRIMARY KEY (`permission_id`,`model_id`,`model_type`),
+  ADD KEY `model_has_permissions_model_id_model_type_index` (`model_id`,`model_type`);
+
+--
+-- Indexes for table `model_has_roles`
+--
+ALTER TABLE `model_has_roles`
+  ADD PRIMARY KEY (`role_id`,`model_id`,`model_type`),
+  ADD KEY `model_has_roles_model_id_model_type_index` (`model_id`,`model_type`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `permissions_name_guard_name_unique` (`name`,`guard_name`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `roles_name_guard_name_unique` (`name`,`guard_name`);
+
+--
+-- Indexes for table `role_has_permissions`
+--
+ALTER TABLE `role_has_permissions`
+  ADD PRIMARY KEY (`permission_id`,`role_id`),
+  ADD KEY `role_has_permissions_role_id_foreign` (`role_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `class_types`
+--
+ALTER TABLE `class_types`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `colleges`
+--
+ALTER TABLE `colleges`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `departments`
+--
+ALTER TABLE `departments`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `class_types`
+--
+ALTER TABLE `class_types`
+  ADD CONSTRAINT `class_types_dept_id_foreign` FOREIGN KEY (`dept_id`) REFERENCES `departments` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `departments`
+--
+ALTER TABLE `departments`
+  ADD CONSTRAINT `departments_college_id_foreign` FOREIGN KEY (`college_id`) REFERENCES `colleges` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `model_has_permissions`
+--
+ALTER TABLE `model_has_permissions`
+  ADD CONSTRAINT `model_has_permissions_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `model_has_roles`
+--
+ALTER TABLE `model_has_roles`
+  ADD CONSTRAINT `model_has_roles_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `role_has_permissions`
+--
+ALTER TABLE `role_has_permissions`
+  ADD CONSTRAINT `role_has_permissions_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `role_has_permissions_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
