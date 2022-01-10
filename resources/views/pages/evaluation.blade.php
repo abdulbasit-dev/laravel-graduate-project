841bfa4f196ccdb3f6e7110789e87d2eb8cbe18e
abdulbasit-dev <abdullbasit99salah@gmail.com>
2022-01-10 08:59:25 +0300

    form

--- resources/views/pages/evaluation.blade.php
+++ resources/views/pages/evaluation.blade.php
@@ -3,55 +3,95 @@
 @section('content')
 
 <style>
-    video {
-  object-fit: cover;
-  width: 100vw;
-  height: 89.6vh;
-  position: absolute;
-  top:20;
-  left: 0;
-}
+    .video-overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, .4);
+        z-index: 2;
+    }
 
+    .home {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        position: relative;
+        padding: 2rem 10rem;
+    }
 
+    .home .video video {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+        z-index: -1;
+    }
 
-.viewport-header {
-  position: relative;
-  height: 100%;
-  text-align: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+    .home .content {
+        margin-top: 6rem;
+        z-index: 10000;
+    }
 
-h1 {
-  font-family: 'Syncopate', sans-serif;
-  color: white;
-  text-transform: uppercase;
-  letter-spacing: 3vw;
-  line-height: 1.2;
-  font-size: 3vw;
-  text-align: center;
-  margin-top: 9rem
-}
-span {
-  display: block;
-  font-size: 10vw;
-  letter-spacing: 1vw;
-}
+    .home .content h1 {
+        font-size: 4.1vw;
+        color: #fff;
+        text-transform: capitalize;
+    }
 
-
-
-
+    .home .content p {
+        font-size: 1.5vw;
+        color: #eee;
+        padding-left: 2rem;
+        border-left: .3rem solid #ddd;
+    }
 </style>
-<video src="{{ asset('images/video.mp4') }}" autoplay loop playsinline muted>
 
-</video>
+<section class="home" id="home">
+    <div class="video-overlay"></div>
+    <div class="video">
+        <video src="{{ asset('images/video.mp4') }}" loop muted autoplay></video>
+    </div>
 
-<header class="viewport-header">
-  <h1>
-    Project
-    <span>Evaluation</span>
-  </h1>
-</header>
+    <div class="content">
+        <h1>
+            explore new places, <br />
+            adventure awaits.
+        </h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod necessitatibus sunt atque
+            consequuntur quam veniam magnam
+            consectetur, odio velit alias!
+        </p>
+    </div>
+</section>
+
+<section class="my-5">
+    <div class="container">
+        <p>
+            <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample"
+                role="button" aria-expanded="false" aria-controls="collapseExample">
+                Link with href
+            </a>
+            <button class="btn btn-primary" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseExample" aria-expanded="false"
+                aria-controls="collapseExample">
+                Button with data-bs-target
+            </button>
+        </p>
+        <div class="collapse" id="collapseExample">
+            <div class="card card-body">
+                Some placeholder content for the collapse component. This panel is hidden by default
+                but
+                revealed when the user activates the relevant trigger.
+            </div>
+        </div>
+
+    </div>
+</section>
 
 @endsection
