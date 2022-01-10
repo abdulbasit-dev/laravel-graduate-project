618ce334fd2d4c66cb63f651a4ef5355fb1173d4
abdulbasit-dev <abdullbasit99salah@gmail.com>
2022-01-10 10:10:31 +0300

    add form

--- resources/views/pages/evaluation.blade.php
+++ resources/views/pages/evaluation.blade.php
@@ -51,7 +51,7 @@
     }
 </style>
 
-<section class="home" id="home">
+<div class="home" id="home">
     <div class="video-overlay"></div>
     <div class="video">
         <video src="{{ asset('images/video.mp4') }}" loop muted autoplay></video>
@@ -68,26 +68,65 @@
             consectetur, odio velit alias!
         </p>
     </div>
-</section>
+</div>
 
-<section class="my-5">
+<section class="mt-5" style="margin-bottom: 20rem">
     <div class="container">
-        <p>
-            <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample"
-                role="button" aria-expanded="false" aria-controls="collapseExample">
-                Link with href
-            </a>
-            <button class="btn btn-primary" type="button" data-bs-toggle="collapse"
-                data-bs-target="#collapseExample" aria-expanded="false"
-                aria-controls="collapseExample">
-                Button with data-bs-target
-            </button>
-        </p>
-        <div class="collapse" id="collapseExample">
-            <div class="card card-body">
-                Some placeholder content for the collapse component. This panel is hidden by default
-                but
-                revealed when the user activates the relevant trigger.
+        <div class="row">
+            <p>
+                <button class="btn btn-primary" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseExample" aria-expanded="false"
+                    aria-controls="collapseExample">
+                    Submit Your Idea
+                </button>
+
+                <button class="btn btn-primary" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseExample1" aria-expanded="false"
+                    aria-controls="collapseExample">
+                    Choose a project
+                </button>
+            </p>
+            <div class="collapse" id="collapseExample">
+                <div class="card card-body">
+                    Some placeholder content for the collapse component. This panel is hidden by
+                    default
+                    but
+                    revealed when the user activates the relevant trigger.
+                </div>
+            </div>
+            <div class="collapse" id="collapseExample1">
+                <div class="card card-body" dir="rtl">
+                    <form>
+                        <div class="row g-3 align-items-center mb-4">
+                            <div class="col-auto">
+                                <label for="inputPassword6" class="col-form-label">ناونیشانی پرۆژەی دەرچوون:</label>
+                            </div>
+                            <div class="col-auto">
+                                <input type="text" id="inputPassword6" class="form-control"
+                                    aria-describedby="passwordHelpInline">
+                            </div>
+                        </div>
+                        <div class="row g-3 align-items-center mb-4">
+                                <div class="col-auto">
+                                    <label for="inputPassword6" class="col-form-label">ناونیشانی پرۆژەی دەرچوون:</label>
+                                </div>
+                                <div class="col-auto">
+                                    <input type="text" id="inputPassword6" class="form-control"
+                                        aria-describedby="passwordHelpInline">
+                                </div>
+                            </div>
+                                 <div class="row g-3 align-items-center mb-4">
+                            <div class="col-auto">
+                                <label for="inputPassword6" class="col-form-label">ناونیشانی پرۆژەی دەرچوون:</label>
+                            </div>
+                            <div class="col-auto">
+                                <input type="text" id="inputPassword6" class="form-control"
+                                    aria-describedby="passwordHelpInline">
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
         </div>
 
