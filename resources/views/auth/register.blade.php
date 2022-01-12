f6318e5c79ea0551b9b59ddcddc3326602b5854a
abdulbasit-dev <abdullbasit99salah@gmail.com>
2022-01-12 22:30:52 +0300

    fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,15 +1,12 @@
 @extends('admin.layouts.guest')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-6 d-flex ">
-            <div class="">
-                <img src="{{ asset('images/register.jpg') }}" alt="register">
+    <div class="container" style="height: 80vh">
+        <div class="d-flex" style="height: 100%;">
+            <div style="height: 100%; width:50% !important;">
+                <img src="{{ asset('images/register.jpg') }}" style="width:100%; height:100% " alt="register">
             </div>
-        </div>
-        <div class="col-6 d-flex align-items-center justify-content-center">
-            <div class="bg-white shadow border-0 rounded border-light p-4 p-lg-5 w-100 fmxw-500">
+            <div class="bg-white shadow border-0 rounded border-light p-4 p-lg-5" style="width:50% !important;">
                 <div class="text-center text-md-center mb-4 mt-md-0">
                     <h1 class="mt-n3 mb-0 h3">{{ __('Create Account') }}</h1>
                 </div>
@@ -24,8 +21,7 @@
                                 <i class="fas fa-user-alt fa-fw"></i>
                             </span>
                             <input name="name" id="name" type="name" class="form-control"
-                                placeholder="{{ __('Name') }}" value="{{ old('name') }}" autofocus
-                                required>
+                                placeholder="{{ __('Name') }}" value="{{ old('name') }}" autofocus required>
                         </div>
 
                         @error('name') <div class="invalid-feedback"> {{ $message }} </div>
@@ -38,19 +34,16 @@
                         <label for="email">{{ __('Your Email') }}</label>
                         <div class="input-group">
                             <span class="input-group-text" id="basic-addon1">
-                                <svg class="icon icon-xs text-gray-600" fill="currentColor"
-                                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z">
+                                <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z">
                                     </path>
-                                    <path
-                                        d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z">
+                                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z">
                                     </path>
                                 </svg>
                             </span>
                             <input name="email" id="email" type="email" class="form-control"
-                                placeholder="{{ __('Email') }}" value="{{ old('email') }}" autofocus
-                                required>
+                                placeholder="{{ __('Email') }}" value="{{ old('email') }}" autofocus required>
                         </div>
                         @error('email') <div class="invalid-feedback"> {{ $message }} </div>
                         @enderror
@@ -62,17 +55,16 @@
                         <label for="password">{{ __('Your Password') }}</label>
                         <div class="input-group">
                             <span class="input-group-text" id="basic-addon2">
-                                <svg class="icon icon-xs text-gray-600" fill="currentColor"
-                                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd"
                                         d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
                                         clip-rule="evenodd">
                                     </path>
                                 </svg>
                             </span>
-                            <input name="password" type="password"
-                                placeholder="{{ __('Password') }}" class="form-control"
-                                id="password" required autocomplete="new-password">
+                            <input name="password" type="password" placeholder="{{ __('Password') }}"
+                                class="form-control" id="password" required autocomplete="new-password">
                         </div>
                         @error('password') <div class="invalid-feedback"> {{ $message }} </div>
                         @enderror
@@ -83,8 +75,8 @@
                         <label for="password_confirmation">{{ __('Confirm Password') }}</label>
                         <div class="input-group">
                             <span class="input-group-text" id="basic-addon3">
-                                <svg class="icon icon-xs text-gray-600" fill="currentColor"
-                                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd"
                                         d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
                                         clip-rule="evenodd">
@@ -111,5 +103,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
