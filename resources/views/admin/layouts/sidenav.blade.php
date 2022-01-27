b496418b592312c947a2b8fe85bdac68bfb30126
abdulbasit-dev <abdullbasit99salah@gmail.com>
2022-01-27 08:22:42 +0300

    navbar fix

--- resources/views/admin/layouts/sidenav.blade.php
+++ resources/views/admin/layouts/sidenav.blade.php
@@ -1,5 +1,5 @@
 <nav id="sidebarMenu" class="sidebar d-lg-block bg-gray-800 text-white collapse" data-simplebar>
-    <div class="sidebar-inner px-2 pt-3">
+    <div class="sidebar-inner px-2 pt-3" style="overflowX: hidden">
         @include('admin.layouts.responsive-topbar')
         @include('admin.layouts.navigation')
     </div>
