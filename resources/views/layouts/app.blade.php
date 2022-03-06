65024e01bf54354e9abb5b7dd339e9795aba46e0
abdulbasit-dev <abdullbasit99salah@gmail.com>
2022-03-06 19:36:33 +0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,13 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- BootStrap -->
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/bootstrap.css') }}">
 
     <!-- fontawesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
 
     {{-- custome style --}}
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <link rel="stylesheet" href="css/custom.css">
 
     @if (config('app.locale') !== 'en')
     <link rel="stylesheet"
