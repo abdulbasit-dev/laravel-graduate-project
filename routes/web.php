85824338cd1b6ba287a26ca50c09a62760cce078
abdulbasit-dev <abdullbasit99salah@gmail.com>
2022-03-22 19:12:56 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use \App\Http\Controllers\{
 
 use \App\Http\Controllers\Admin\{
     CollegeController,
+    DashboradController,
     DepartmentController,
     TeamController,
     BannerController,
